9dd6645ef484b6b097fe9ede8de8c0af6c7ee695
Yuri Peshekhonov <yuripesh@gmail.com>
2022-02-08 12:12:45 +0300

    First Commit
    Initial state

---
+++ TestProject1/TestProject1/Script/Unit1.js
@@ -0,0 +1,21 @@
+﻿function Test()
+{
+  Log.Enabled = false;
+  Log.Error("process crashed")
+  Log.Enabled = true; 
+}
+
+//@edge 
+function EventControl1_OnLogError(Sender, LogParams)
+{
+  if (equal(LogParams.MessageText, "process crashed"))
+  {
+    
+  }
+  else
+  {
+    // If another error occurs,
+    // Stop the current test item.
+    Runner.Stop(true);
+  }
+}
